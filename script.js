65b503304aa51523de5187d153c9d14dc1e25e1f
Natan Falconi <nfalconi65@gmail.com>
2021-01-22 14:48:57 -0300

    refatoração do arquivo modal

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const modal = document.querySelector('.modal-overlay')
-const button = document.querySelector('.button')
-const close = document.querySelector('.cancel')
+// const button = document.querySelector('.button')
+// const close = document.querySelector('.cancel')
 
-button.addEventListener('click', () => {
-    modal.classList.add('active')
+// button.addEventListener('click', () => {
+//     modal.classList.add('active')
 
-})
+// })
 
-close.addEventListener('click', () => {
-    modal.classList.remove('active')
+// close.addEventListener('click', () => {
+//     modal.classList.remove('active')
 
-})
+// })
+
+
+var Modal = {
+    open(){
+        modal.classList.add('active')
+    },
+    close(){
+        modal.classList.remove('active')
+    }
+}
